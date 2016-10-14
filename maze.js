81da8aa7fc985a892a4f81e7726d784b63b108e0
Akinyele Thompson <Akinyele Thompson>
2016-10-13 22:17:54 -0500

    Lab4 Q2

--- maze.js
+++ maze.js
@@ -6,13 +6,13 @@
 window.onload = function() {
 
 
-hitboundary();
+hitBoundary();
 
 };
 
 
 
-function hitboundary() {
+function hitBoundary() {
 
 //alert("load");
 
@@ -21,34 +21,46 @@ var wall = document.getElementById("boundary1");
 var walls = document.getElementsByClassName("boundary");
 
 wall.addEventListener("mouseover", function(){
-wall.setAttribute("class" , "boundary youlose");
+//walls.setAttribute("class" , "boundary youlose");
+redBoundary();
 });
 
 
 walls[1].addEventListener("mouseover", function(){
-    
-    walls[1].setAttribute("class", "boundary youlose");
-    
+        // walls[1].setAttribute("class", "boundary youlose");
+         redBoundary();
+   
 });
 
 walls[2].addEventListener("mouseover", function(){
-    
-    walls[2].setAttribute("class", "boundary youlose");
-    
-});walls[3].addEventListener("mouseover", function(){
-    
-    walls[3].setAttribute("class", "boundary youlose");
-    
+       // walls[2].setAttribute("class", "boundary youlose");
+        redBoundary();
+});
+
+walls[3].addEventListener("mouseover", function(){
+    //    walls[3].setAttribute("class", "boundary youlose");
+        redBoundary();
 });
 
 walls[4].addEventListener("mouseover", function(){
-    
-    walls[4].setAttribute("class", "boundary youlose");
-    
+    //    walls[4].setAttribute("class", "boundary youlose");
+    redBoundary();
 });
 
 
 }
 
+
+function redBoundary() {
+    
+    //alert("load");
+    var walls = document.getElementsByClassName("boundary");
+    
+    for (var i = 0; i < walls.length; i++) {
+    walls[i].setAttribute("class", "boundary youlose");
+        
+    }
+    
+}
 
 
